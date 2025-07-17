03ef4f7c2966ed5f66e5e8d7e03b4e7065759f21
mahinmohd <mabbasmahin@gmail.com>
2025-07-17 14:28:18 +0100

    Initial Commit

---
+++ LearnMetalShaders/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnMetalShaders
+//
+//  Created by Mohammed Abbas Mahin on 17/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnMetalShaders/LearnMetalShadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMetalShadersApp.swift
+//  LearnMetalShaders
+//
+//  Created by Mohammed Abbas Mahin on 17/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearnMetalShadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
